4989d03fff2f59b67e63e157851715ab26540a2b
Joshua Liu <liujoshua@users.noreply.github.com>
2020-05-18 17:38:38 -0700

    KTOR

--- SharedCode/build.gradle.kts
+++ SharedCode/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import dev.mobilehealth.reimaginedlamp.gradle.BuildConfig
 
 plugins {
     kotlin("multiplatform")
@@ -24,6 +25,9 @@ kotlin {
 
     sourceSets["commonMain"].dependencies {
         implementation("org.jetbrains.kotlin:kotlin-stdlib-common")
+
+        // KTOR
+        implementation("io.ktor:ktor-client-core:${BuildConfig.ktorVersion}")
     }
 
     sourceSets["commonTest"].dependencies {
@@ -33,6 +37,9 @@ kotlin {
 
     sourceSets["androidMain"].dependencies {
         implementation("org.jetbrains.kotlin:kotlin-stdlib")
+
+        // KTOR
+        implementation("io.ktor:ktor-client-android:${BuildConfig.ktorVersion}")
     }
 
     sourceSets["androidTest"].dependencies {
@@ -42,6 +49,9 @@ kotlin {
 
     sourceSets["iosMain"].dependencies {
         api( "org.jetbrains.kotlinx:kotlinx-serialization-runtime-native:0.20.0")
+
+        // KTOR
+        implementation("io.ktor:ktor-client-ios:${BuildConfig.ktorVersion}")
     }
 }
 
