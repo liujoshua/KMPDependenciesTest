1ecd3a2f9c9c680b981605f09f44c17603340908
Joshua Liu <liujoshua@users.noreply.github.com>
2020-05-18 22:05:01 -0700

    Add java 1.8 compile options

--- SharedCode/build.gradle.kts
+++ SharedCode/build.gradle.kts
@@ -21,6 +21,17 @@ android {
         }
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
+        
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
 }
 kotlin {
 
