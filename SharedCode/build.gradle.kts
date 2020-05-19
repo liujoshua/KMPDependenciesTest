12d40bdf2443844f445c1b41bd31b716b965c3cf
Joshua Liu <liujoshua@users.noreply.github.com>
2020-05-18 17:45:42 -0700

    Add Import

--- SharedCode/build.gradle.kts
+++ SharedCode/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import dev.mobilehealth.reimaginedlamp.gradle.BuildConfig
 
 plugins {
     kotlin("multiplatform")
@@ -24,6 +25,7 @@ kotlin {
 
     sourceSets["commonMain"].dependencies {
         implementation("org.jetbrains.kotlin:kotlin-stdlib-common")
+
     }
 
     sourceSets["commonTest"].dependencies {
@@ -41,7 +43,7 @@ kotlin {
     }
 
     sourceSets["iosMain"].dependencies {
-        api( "org.jetbrains.kotlinx:kotlinx-serialization-runtime-native:0.20.0")
+        api("org.jetbrains.kotlinx:kotlinx-serialization-runtime-native:0.20.0")
     }
 }
 
@@ -64,10 +66,12 @@ val packForXcode by tasks.creating(Sync::class) {
     /// generate a helpful ./gradlew wrapper with embedded Java path
     doLast {
         val gradlew = File(targetDir, "gradlew")
-        gradlew.writeText("#!/bin/bash\n"
-                + "export 'JAVA_HOME=${System.getProperty("java.home")}'\n"
-                + "cd '${rootProject.rootDir}'\n"
-                + "./gradlew \$@\n")
+        gradlew.writeText(
+            "#!/bin/bash\n"
+                    + "export 'JAVA_HOME=${System.getProperty("java.home")}'\n"
+                    + "cd '${rootProject.rootDir}'\n"
+                    + "./gradlew \$@\n"
+        )
         gradlew.setExecutable(true)
     }
 }
