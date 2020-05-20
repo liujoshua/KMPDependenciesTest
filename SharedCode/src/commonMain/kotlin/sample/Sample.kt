51b5528de105f02e4ee0db98d74df677f7673878
Joshua Liu <liujoshua@users.noreply.github.com>
2020-05-19 22:13:19 -0700

    Fix caching

--- SharedCode/src/commonMain/kotlin/sample/Sample.kt
+++ SharedCode/src/commonMain/kotlin/sample/Sample.kt
@@ -1,5 +1,9 @@
 package sample
 
+import com.benasher44.uuid.uuid4
+import com.soywiz.klock.DateFormat
+import com.soywiz.klock.DateTime
+
 import dev.icerock.moko.mvvm.livedata.LiveData
 import dev.icerock.moko.mvvm.livedata.MutableLiveData
 
@@ -13,6 +17,14 @@ expect object Platform {
 
 fun hello(): String = "Hello from ${Platform.name()}"
 
-fun getHelloLiveData():LiveData<String> {
+fun sayUuid(): String {
+    return uuid4().toString()
+}
+
+fun sayDate(): String {
+    return DateTime.now().toString(DateFormat.FORMAT1)
+}
+
+fun getHelloLiveData(): LiveData<String> {
     return MutableLiveData(hello())
 }
