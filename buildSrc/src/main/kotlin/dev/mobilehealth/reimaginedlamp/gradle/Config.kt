8798ab172082f26b339226a53ded956aeb36fa68
Joshua Liu <liujoshua@users.noreply.github.com>
2020-05-19 19:56:52 -0700

    Kotlin 1.3.70

--- buildSrc/src/main/kotlin/dev/mobilehealth/reimaginedlamp/gradle/Config.kt
+++ buildSrc/src/main/kotlin/dev/mobilehealth/reimaginedlamp/gradle/Config.kt
@@ -6,7 +6,7 @@ object BuildConfig {
     val compileSdkVersion = targetSdkVersion
 
     val buildToolsVersion = "28.0.3"
-    val kotlinVersion = "1.3.71"
+    val kotlinVersion = "1.3.70"
 
     val mokkoMvvmVersion = "0.6.0"
     val androidLifecycleVersion = "0.2.0"
