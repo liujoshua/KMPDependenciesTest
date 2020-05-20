db5adc98672b16efeb7ba5bfaaf3484b1ec0b69d
Joshua Liu <liujoshua@users.noreply.github.com>
2020-05-20 10:10:00 -0700

    Add atomicfu to commonTest

--- SharedCode/build.gradle.kts
+++ SharedCode/build.gradle.kts
@@ -73,6 +73,8 @@ kotlin {
     sourceSets["commonTest"].dependencies {
         implementation(kotlin("test-common"))
         implementation(kotlin("test-annotations-common"))
+
+        implementation("org.jetbrains.kotlinx:atomicfu-common:0.14.2")
     }
 
     sourceSets["androidMain"].dependencies {
