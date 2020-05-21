22fa36d6223964f3ea7c0ba35ab3d9d04cb4f52b
josh <josh@Wormwood-2.local>
2020-05-20 18:02:37 -0700

    Builds on emulator and device using cocoapods

--- SharedCode/build.gradle.kts
+++ SharedCode/build.gradle.kts
@@ -4,6 +4,7 @@ import dev.mobilehealth.reimaginedlamp.gradle.BuildConfig
 plugins {
     id("com.android.library")
     kotlin("multiplatform")
+    kotlin("native.cocoapods")
 }
 
 android {
@@ -40,20 +41,33 @@ kotlin {
             publishLibraryVariants("release", "debug")
         }
     }
-    //select iOS target platform depending on the Xcode environment variables
-    val iOSTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
-        if (System.getenv("SDK_NAME")?.startsWith("iphoneos") == true)
-            ::iosArm64
-        else
-            ::iosX64
 
-    iOSTarget("ios") {
-        binaries {
-            framework {
-                baseName = "SharedCode"
-            }
-        }
+    val buildForDevice = project.findProperty("kotlin.native.cocoapods.target") == "ios_arm"
+    if (buildForDevice) {
+        iosArm64("ios64")
+        iosArm32("ios32")
+
+        val iOSMain by sourceSets.creating
+        sourceSets["ios64Main"].dependsOn(iOSMain)
+        sourceSets["ios32Main"].dependsOn(iOSMain)
+    } else {
+        iosX64("ios")
     }
+
+    //select iOS target platform depending on the Xcode environment variables
+//    val iOSTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
+//        if (System.getenv("SDK_NAME")?.startsWith("iphoneos") == true)
+//            ::iosArm64
+//        else
+//            ::iosX64
+//
+//    iOSTarget("ios") {
+//        binaries {
+//            framework {
+//                baseName = "SharedCode"
+//            }
+//        }
+//    }
 
     sourceSets {
         commonMain {
@@ -117,36 +131,46 @@ kotlin {
         // KTOR
 //        implementation("io.ktor:ktor-client-ios:${BuildConfig.ktorVersion}")
     }
-}
 
-val packForXcode by tasks.creating(Sync::class) {
-    val targetDir = File(buildDir, "xcode-frameworks")
 
-    /// selecting the right configuration for the iOS 
-    /// framework depending on the environment
-    /// variables set by Xcode build
-    // or XCODE_CONFIGURATION
-    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
-    val framework = kotlin.targets
-        .getByName<KotlinNativeTarget>("ios")
-        .binaries.getFramework(mode)
-    inputs.property("mode", mode)
-    dependsOn(framework.linkTask)
 
-    from({ framework.outputDirectory })
-    into(targetDir)
-
-    /// generate a helpful ./gradlew wrapper with embedded Java path
-    doLast {
-        val gradlew = File(targetDir, "gradlew")
-        gradlew.writeText(
-            "#!/bin/bash\n"
-                    + "export 'JAVA_HOME=${System.getProperty("java.home")}'\n"
-                    + "cd '${rootProject.rootDir}'\n"
-                    + "./gradlew \$@\n"
-        )
-        gradlew.setExecutable(true)
+    cocoapods {
+        summary = "Working with cocoapods"
+        homepage = "https://github.com/liujoshua/reimagined-lamp"
+        pod("SharedCode", "~> 3.2.0")
     }
 }
 
-tasks.getByName("build").dependsOn(packForXcode)
+
+//
+//val packForXcode by tasks.creating(Sync::class) {
+//    val targetDir = File(buildDir, "xcode-frameworks")
+//
+//    /// selecting the right configuration for the iOS
+//    /// framework depending on the environment
+//    /// variables set by Xcode build
+//    // or XCODE_CONFIGURATION
+//    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
+//    val framework = kotlin.targets
+//        .getByName<KotlinNativeTarget>("ios")
+//        .binaries.getFramework(mode)
+//    inputs.property("mode", mode)
+//    dependsOn(framework.linkTask)
+//
+//    from({ framework.outputDirectory })
+//    into(targetDir)
+//
+//    /// generate a helpful ./gradlew wrapper with embedded Java path
+//    doLast {
+//        val gradlew = File(targetDir, "gradlew")
+//        gradlew.writeText(
+//            "#!/bin/bash\n"
+//                    + "export 'JAVA_HOME=${System.getProperty("java.home")}'\n"
+//                    + "cd '${rootProject.rootDir}'\n"
+//                    + "./gradlew \$@\n"
+//        )
+//        gradlew.setExecutable(true)
+//    }
+//}
+//
+//tasks.getByName("build").dependsOn(packForXcode)
