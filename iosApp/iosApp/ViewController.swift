5b9e91cfe1224e24110265229ed01ebfe9ed9343
josh <josh@Wormwood-2.local>
2020-05-21 02:44:29 -0700

    Scans a QR code

--- iosApp/iosApp/ViewController.swift
+++ iosApp/iosApp/ViewController.swift
@@ -2,16 +2,22 @@ import UIKit
 //import app
 import SharedCode
 import MaterialComponents
-
 class ViewController: UIViewController {
+
+        @IBOutlet weak var label: UILabel!
+     @IBOutlet weak var bottomNavBar: MDCBottomNavigationBar!
+    @IBAction func floatingActionButton(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 //        label.text = Proxy().proxyHello()
         
     }
 
+    
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    @IBOutlet weak var label: UILabel!
 }
